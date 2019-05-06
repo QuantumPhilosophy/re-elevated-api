ef60762607e733f8ed0bc76ca9a989f478b35243
MonteCristo <mjblee20@gmail.com>
2019-05-06 11:50:30 -0700

    routes WIP

--- routes/index.js
+++ routes/index.js
@@ -16,8 +16,27 @@ module.exports = function (app) {
       res.json(results);
     })
   });
+
   // Add new item to wishlist (post)
+  app.post('/:id/wishlisted', function (req, res) {
+    db.Wishlisted_Strain.create({
+      where: { 
+        user_id: req.params.id,
+        strain_id: req.body.strainId
+      },
+    }).then(results => {
+      res.json(results);
+    })
+  });
+
   // Remove item from wishlist
+  app.delete('/:wishid', function (req, res) {
+    db.Wishlisted_Strain.destroy({
+      where: { id: req.params.wishid }
+    }).then(results => {
+      res.json(results);
+    })
+  });
 
   // Get all tried items of the logged in user
   app.get('/:id/tried', function (req, res) {
@@ -30,23 +49,57 @@ module.exports = function (app) {
       res.json(results);
     })
   });
+
   // Add new item to tried list (post)
+  app.post('/:id/tried', function (req, res) {
+    db.Tried_Strain.create({
+      where: { 
+        user_id: req.params.id,
+        strain_id: req.body.strainId
+      },
+    }).then(results => {
+      res.json(results);
+    })
+  });
+  
   // Remove item from tried list
+  app.delete('/:triedid', function (req, res) {
+    db.Tried_Strain.destroy({
+      where: { id: req.params.triedid }
+    }).then(results => {
+      res.json(results);
+    })
+  });
 
   // Get all strain reviews written by the logged in user
   app.get('/:id/strainreviews', function (req, res) {
-    db.User.findAll({
-      where: { id: req.params.id },
+    db.Strain_Review.findAll({
+      where: { user_id: req.params.id },
       include: [
-        { model: db.Strain_Review },
+        { model: db.User },
       ]
     }).then(results => {
       res.json(results);
     })
   });
+
   // Add new review to an item (post)
+  app.post('/:id/strainreviews', function (req, res) {
+    db.Strain_Review.create({
+      where: { 
+        user_id: req.params.id,
+        strain_id: req.body.strainId
+      },
+    }).then(results => {
+      res.json(results);
+    })
+  });
+
   // Update already existing reviews
+  app.put();
+
   // Remove a review written
+  app.delete();
 
   // Get all merchant reviews written by the logged in user
   app.get('/:id/merchantreviews', function (req, res) {
