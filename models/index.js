16f5ab223678e69868a2f49ef49293e1c4b3872f
QuantumPhilosopher <16587451+QuantumPhilosophy@users.noreply.github.com>
2019-05-08 15:50:18 -0700

    updates to jawsDB settings

--- models/index.js
+++ models/index.js
@@ -10,13 +10,12 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-fs
-  .readdirSync(__dirname)
+fs.readdirSync(__dirname)
   .filter(file => {
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
