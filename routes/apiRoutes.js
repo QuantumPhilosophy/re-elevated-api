681f93e1517cefd33f1edba8a375c75bcbdc627b
MonteCristo <mjblee20@gmail.com>
2019-05-09 11:30:01 -0700

    WIP update avgRating when new review is added

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,84 +2,109 @@ const db = require("../models");
 
 module.exports = function (app) {
 
+  function calcAvg(ratingArr) {
+    // iterate through all the ratings and sum them up
+    let sum = 0;
+    for (let i = 0; i < ratingArr.length; i++) {
+      sum += ratingArr[i];
+    }
+    let avgRating = sum / ratingArr.length;
+    return avgRating;
+  };
+
   app.get('/', function (req, res) {
     res.json('Welcome to Elevated!')
   })
 
   // All of User's Use Cases
   // ==============================
-  // Adding new User to DB
-  app.post('/auth/user/signup', function (req, res) {
-    db.User.create({
-      user_name: req.body.name,
-      user_email: req.body.email,
-      user_password: req.body.password,
-      dob: req.body.dob
-    }).then(results => {
-      res.json(results);
-    })
-  });
+
+  // below code should be in authRoutes.js
+
+  // // Adding new User to DB
+  // app.post('/auth/user/signup', function (req, res) {
+  //   db.User.create({
+  //     user_name: req.body.name,
+  //     user_email: req.body.email,
+  //     user_password: req.body.password,
+  //     dob: req.body.dob
+  //   }).then(results => {
+  //     res.json(results);
+  //   })
+  // });
 
   // Get all wishlisted items of the logged in user
-  app.get('/wishlisted/:userId', function (req, res) {
+  app.get('/wishlisted/:user_id', function (req, res) {
     db.Wishlisted_Strain.findAll({
-      where: { user_id: req.params.userId }
+      where: { user_id: req.params.user_id }
     }).then(results => {
       res.json(results);
     })
   });
 
   // Add new item to wishlist (post)
-  app.post('/wishlisted/:userId', function (req, res) {
+  app.post('/wishlisted/:user_id', function (req, res) {
     db.Wishlisted_Strain.create({
-      user_id: req.params.userId,
-      strain_id: req.body.strainId
+      user_id: req.params.user_id,
+      strain_id: req.body.strain_id,
+      label_id: req.body.label_id
     }).then(results => {
       res.json(results);
     })
   });
 
   // Remove item from wishlist
-  app.delete('/wishlisted/:wishId', function (req, res) {
+  app.delete('/wishlisted/:wish_id', function (req, res) {
     db.Wishlisted_Strain.destroy({
-      where: { id: req.params.wishId }
+      where: { 
+        id: req.params.wish_id,
+        user_id: req.body.user_id 
+      }
     }).then(results => {
       res.json(results);
     })
   });
 
   // Get all tried items of the logged in user
-  app.get('/tried/:userId', function (req, res) {
+  app.get('/tried/:user_id', function (req, res) {
     db.Tried_Strain.findAll({
-      where: { user_id: req.params.userId }
+      where: { 
+        user_id: req.params.user_id 
+      }
     }).then(results => {
       res.json(results);
     })
   });
 
   // Add new item to tried list (post)
-  app.post('/tried/:userId', function (req, res) {
+  app.post('/tried/:user_id', function (req, res) {
     db.Tried_Strain.create({
-      user_id: req.params.userId,
-      strain_id: req.body.strainId
+      user_id: req.params.user_id,
+      strain_id: req.body.strain_id,
+      label_id: req.body.label_id
     }).then(results => {
       res.json(results);
     })
   });
   
   // Remove item from tried list
-  app.delete('/tried/:triedId', function (req, res) {
+  app.delete('/tried/:tried_id', function (req, res) {
     db.Tried_Strain.destroy({
-      where: { id: req.params.triedId }
+      where: { 
+        id: req.params.tried_id ,
+        user_id: req.body.user_id 
+      }
     }).then(results => {
       res.json(results);
     })
   });
 
   // Get all strain reviews written by the logged in user
-  app.get('/strainreviews/:userId', function (req, res) {
+  app.get('/strainreviews/:user_id', function (req, res) {
     db.Strain_Review.findAll({
-      where: { user_id: req.params.userId }
+      where: { 
+        user_id: req.params.user_id 
+      }
     }).then(results => {
       res.json(results);
     })
@@ -87,90 +112,109 @@ module.exports = function (app) {
 
   // Add new review to a strain (post)
   app.post('/strainreviews/:user_id', function (req, res) {
+    // create new instance of a review
     db.Strain_Review.create({
       user_id: req.params.user_id,
       strain_id: req.body.strain_id,
-      label_id: req.body.user_id
+      label_id: req.body.user_id,
+      strain_label_rating: req.body.strain_label_rating
     }).then(results => {
-      // grab the rating of the newly created review 
-      // update the strain_rating in strain table
-      // scrap all rows with the 
+      // grab the all rating of the strain that the new review is about
       db.Strain_Review.findAll({
         where: {
-          strain_id: req.body.strain_id
+          strain_id: results.strain_id
         },
         // selects the 
-        attributes: ['id', count[strain_id]]
+        attributes: ['strain_label_rating']
+      }).then(results => {
+        // update the strain_rating in strain table
+        // results should be an array of numbers which is the rating given in all reviews
+        console.log(results); // this should be an array of ratings
+        let newRating = calcAvg(results)
+        db.Strain.update({
+          strain_avg_rating: newRating
+        }, { 
+          where: { strain_id: req.body.strain_id } 
+        }).then(results => {
+          return res.json(results);
+        })
       })
       // update the label_rating in the lables table
       // scrap all rows with the 
-      db.Strain_Review.findAll({
-        where: {
-          label_id: req.body.label_id
-        }
+      // db.Strain_Review.findAll({
+      //   where: {
+      //     label_id: req.body.label_id
+      //   }
 
-      })
+      // })
       
-      res.json(results);
+      // res.json(results);
     })
   });
 
   // Update already existing reviews
-  app.put('/strainreviews/:reviewId', function (req, res) {
+  app.put('/strainreviews/:review_id', function (req, res) {
     db.Strain_Review.update({
 
     }, {
-      where: { id: req.params.reviewId }
+      where: { id: req.params.review_id }
     }).then(results => {
       res.json(results);
     })
   });
 
   // Remove a review written
-  app.delete('/strainreviews/remove/:reviewId', function (req, res) {
+  app.delete('/strainreviews/remove/:review_id', function (req, res) {
     db.Strain_Review.destroy({
-      where: { id: req.params.reviewId }
+      where: { id: req.params.review_id }
     }).then(results => {
       res.json(results);
     })
   });
 
   // Get all merchant reviews written by the logged in user
-  app.get('/merchantreviews/:userId', function (req, res) {
+  app.get('/merchantreviews/:user_id', function (req, res) {
     db.Merchant_Review.findAll({
-      where: { user_id: req.params.userId }
+      where: { user_id: req.params.user_id }
     }).then(results => {
       res.json(results);
     })
   });
 
   // Add new review to a merchant (post)
-  app.post('/merchantreviews/:userId', function (req, res) {
+  app.post('/merchantreviews/:user_id', function (req, res) {
     db.Merchant_Review.create({
-      user_id: req.params.userId,
-      merchant_id: req.body.merchantId,
-      merchant_review: req.body.merchantReview,
-      merchant_rating: req.body.merchantRating
+      user_id: req.params.user_id,
+      merchant_id: req.body.merchant_id,
+      merchant_review: req.body.merchant_review,
+      merchant_rating: req.body.merchant_rating
     }).then(results => {
+      // update merchant's ratings
+      // grabbing all ratings of the merchant with the given id
+      console.log(results);
+      db.Merchant_Review.findAll({
+        where: { id: results.merchant_id }
+      })
+
       res.json(results);
     })
   });
 
   // Update already existing merchant review
-  app.put('/merchantreviews/update/:userId', function (req, res) {
+  app.put('/merchantreviews/update/:user_id', function (req, res) {
     db.Merchant_Review.update(
       {},
-      { where: { id: req.params.userId } }
+      { where: { id: req.params.user_id } }
     ).then(results => {
       res.json(results);
     })
   });
 
   // Remove a written merchant review 
-  app.delete('/merchantreviews/remove/:merchantId', function (req, res) {
+  app.delete('/merchantreviews/remove/:merchant_id', function (req, res) {
     db.Merchant_Review.destroy({
       where: {
-        id: req.params.merchantId,
+        id: req.params.merchant_id,
       }
     }).then(results => {
       res.json(results);
@@ -181,26 +225,38 @@ module.exports = function (app) {
   // ================================
   // Get all strains available to customers
   app.get('/strains', function (req, res) {
-    db.Strain.findAll({}).then(results => {
-      res.json(results);
-    })
+    db.Strain
+      .findAll({})
+      .then(results => {
+        res.json(results);
+      })
+  });
+
+  // Get all labels in the database 
+  app.get('/labels', function (req, res) {
+    db.Label
+      .findAll({})
+      .then(results => {
+        res.json(results);
+      })
   });
 
   // Adding new strains to the database (post)
   app.post('/strains', function (req, res) {
-    db.Strain.create({
-      strain_name: req.body.strainName,
-      strain_race: req.body.strainRace,
-      strain_flavor: req.body.strainFlavor,
-      strain_positive: req.body.strainPositive,
-      strain_negative: req.body.strainNegative,
-      strain_medical: req.body.strainMedical,
-      strain_descr: req.body.strainDescr,
-      strain_img: req.body.strainImg,
-      strain_avg_rating: req.body.strainAvgRating
-    }).then(results => {
-      res.json(results);
-    })
+    db.Strain
+      .create({
+        strain_name: req.body.strainName,
+        strain_race: req.body.strainRace,
+        strain_flavor: req.body.strainFlavor,
+        strain_positive: req.body.strainPositive,
+        strain_negative: req.body.strainNegative,
+        strain_medical: req.body.strainMedical,
+        strain_descr: req.body.strainDescr,
+        strain_img: req.body.strainImg,
+        strain_avg_rating: req.body.strainAvgRating
+      }).then(results => {
+        res.json(results);
+      })
   });
 
   // All of Merchants's Use Cases
@@ -212,54 +268,57 @@ module.exports = function (app) {
     })
   });
 
-  // Adding new Merchants to DB
-  app.post('/auth/merchant/signup', function (req, res) {
-    db.Merchant.create({
-      merchant_name: req.body.name,
-      merchant_email: req.body.email,
-      merchant_password: req.body.password,
-      merchant_location: req.body.address
-    }).then(results => {
-      res.json(results);
-    })
-  });
+  // THE BELOW CODE HAS BEEN MOVED TO AUTHROUTES.js
+
+  // // Adding new Merchants to DB
+  // app.post('/auth/merchant/signup', function (req, res) {
+  //   db.Merchant.create({
+  //     merchant_name: req.body.name,
+  //     merchant_email: req.body.email,
+  //     merchant_password: req.body.password,
+  //     merchant_location: req.body.address
+  //   }).then(results => {
+  //     res.json(results);
+  //   })
+  // });
 
   // Get all reviews written about this merchant
-  app.get('/merchantreviews/:merchantId', function (req, res) {
+  app.get('/merchantreviews/:merchant_id', function (req, res) {
     db.Merchant_Review.findAll({
-      where: { id: req.params.merchantId },
+      where: { id: req.params.merchant_id },
     }).then(results => {
       res.json(results);
     })
   });
 
   // Get all merchant's Ads 
-  app.get('/merchantads/:merchantId', function (req, res) {
+  app.get('/merchantads/:merchant_id', function (req, res) {
     db.Merchant_Review.findAll({
-      where: { id: req.params.merchantId },
+      where: { id: req.params.merchant_id },
     }).then(results => {
       res.json(results);
     })
   });
 
   // Merchants adding ads (post)
-  app.post('/merchantads/add/:merchantId', function (req, res) {
+  app.post('/merchantads/add/:merchant_id', function (req, res) {
     db.Merchant_Ad.create({
-      merchant_id: req.params.merchantId,
-      ad_img: req.body.adImg
+      merchant_id: req.params.merchant_id,
+      ad_img: req.body.ad_img
     }).then(results => {
       res.json(results);
     })
   });
 
   // Merchant adding growers review (post)
-  app.post('/growerreviews/add/:merchantId', function (req, res) {
+  app.post('/growerreviews/add/:merchant_id', function (req, res) {
     db.Grower_Review.create({
-      merchant_id: req.params.merchantId,
-      grower_id: req.body.growerId,
-      grower_review: req.body.growerReview,
-      grower_rating: req.body.growerRating
+      merchant_id: req.params.merchant_id,
+      grower_id: req.body.grower_id,
+      grower_review: req.body.grower_review,
+      grower_rating: req.body.grower_rating
     }).then(results => {
+      // update grower's rating
       res.json(results);
     })
   });
@@ -273,40 +332,42 @@ module.exports = function (app) {
     })
   });
 
-  // Adding new Growers to DB
-  app.post('/auth/grower/signup', function (req, res) {
-    db.Grower.create({
-      grower_name: req.body.name,
-      grower_email: req.body.email,
-      grower_password: req.body.password
-    }).then(results => {
-      res.json(results);
-    })
-  });
+  // THE BELOW CODE HAS BEEN MOVED TO AUTHROUTES.js
+  
+  // // Adding new Growers to DB
+  // app.post('/auth/grower/signup', function (req, res) {
+  //   db.Grower.create({
+  //     grower_name: req.body.name,
+  //     grower_email: req.body.email,
+  //     grower_password: req.body.password
+  //   }).then(results => {
+  //     res.json(results);
+  //   })
+  // });
 
   // Get all reviews written about this grower
-  app.get('/growerreviews/:growerId', function() {
+  app.get('/growerreviews/:grower_id', function() {
     db.Grower_Review.findAll({
-      where: { id: req.params.growerId }
+      where: { id: req.params.grower_id }
     }).then(results => {
       res.json(results);
     })
   });
 
   // Get grower's menu
-  app.get('/growermenu/:growerId', function (req, res) {
+  app.get('/growermenu/:grower_id', function (req, res) {
     db.Grower_Menu.findAll({
-      where: { id: req.params.growerId }
+      where: { id: req.params.grower_id }
     }).then(results => {
       res.json(results);
     })
   });
 
   // add grower's menu
-  app.post('/growermenu/add/:growerId', function (req, res) {
+  app.post('/growermenu/add/:grower_id', function (req, res) {
     db.Grower_Menu.create({
-      grower_id: req.params.growerId,
-      strain_list: req.body.strainList
+      grower_id: req.params.grower_id,
+      strain_list: req.body.strain_list
     }).then(results => {
       res.json(results);
     })
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -15,12 +15,45 @@ module.exports = function (app) {
   // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
   // otherwise send back an error
-  app.post('/auth/signup', function (req, res) {
+  app.post('/auth/user/signup', function (req, res) {
     db.User.create({
       user_name: req.body.user_name,
       user_email: req.body.user_email,
       user_password: req.body.user_password,
       dob: req.body.dob
+    }).then(function (data) {
+      res.json(data)
+    }).catch(function (err) {
+      res.json(err)
+    })
+  })
+
+  // Route for signing up as a merchant
+  app.post('/auth/merchant/signup', function (req, res) {
+    db.Merchant.create({
+      merchant_name: req.body.merchant_name,
+      merchant_email: req.body.merchant_email,
+      merchant_password: req.body.merchant_password,
+      merchant_verification: req.body.merchant_verification,
+      merchant_verification_img: req.body.merchant_verification_img,
+      merchant_img: req.body.merchant_img,
+      merchant_location: req.body.merchant_location
+    }).then(function (data) {
+      res.json(data)
+    }).catch(function (err) {
+      res.json(err)
+    })
+  })
+
+  // Route for signing up as a grower
+  app.post('/auth/grower/signup', function (req, res) {
+    db.Grower.create({
+      grower_name: req.body.grower_name,
+      grower_email: req.body.grower_email,
+      grower_password: req.body.grower_password,
+      // grower_verification: req.body.grower_verification,
+      grower_verification_img: req.body.grower_verification_img,
+      grower_img: req.body.grower_img,
     }).then(function (data) {
       res.json(data)
     }).catch(function (err) {
